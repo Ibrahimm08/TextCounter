3e029ece07e6faec099d5df58e19a8a0e793deef
Mohammed Maqsood <mohammedmaqsood@Mohammeds-MBP.localdomain>
2025-07-03 14:55:20 +0100

    Character counter

--- Scripts.js
+++ Scripts.js
@@ -11,3 +11,22 @@ function ToggleDarkmode(){
         self.innerText="Sun";
     }
 }
+
+
+function Track(){
+    const TextIn = document.getElementById("TextInput").value;
+    
+    const CharTotal = document.getElementById("CharTotal");
+    const WordTotal = document.getElementById("WordTotal");
+    const SentenceTotal = document.getElementById("SentenceTotal");
+
+    var regex = /[a-zA-Z0-9]/g;
+    console.log(TextIn);
+
+    if(TextIn == ""){
+        CharTotal.innerText = 0;
+    }else{
+        CharTotal.innerText = TextIn.match(regex).length;
+    }
+
+}
